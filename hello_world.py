e1d1213cf6959f4e1108a674fcc8778ce682f623
marina-wang-ETP <78393752+marina-wang-ETP@users.noreply.github.com>
2021-02-02 10:00:33 +0800

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print ("hello world")
+a = 'hello world'
+for i in range (5):
+    if i<6:
+      print (a);
+      
+ 
+list1 = ['physics', 'chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5 ]
+list3 = ["a", "b", "c", "d"]
+list4 = [a,b,c,d] # "" must be used for letter?
+
+print ("list1[course and year]:", list1[0], list1[1])
+print ("list1[course and year]:", list1[0:2] )
+print ("list2[1:5]:",list2[0:5]) # why from 0 to 5?
+print ("list2[1:5]:",list2[0])
+print ("list3[a:d]:", list3[0:4])  
+print ("list3[a:d]:", list3[0])  
+print ("list4[a:d]:", list4[0])
+
+
+
